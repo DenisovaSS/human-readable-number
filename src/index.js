695f4fc7484ffb5d6ab075578f6bb2f127d4c307
DenisovaSS <114432399+DenisovaSS@users.noreply.github.com>
2023-08-13 15:29:31 +0300

    feat: add solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(s) {
+    const dg = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const tn = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tw = [
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+    let str = s.toString();
+    if (str.length == 1) {
+        return str[0] == "0" ? "zero" : dg[s];
+    } else if (str.length == 2) {
+        return (
+            (str[0] != "0" && str[0] != "1" ? "" + tw[+str[0] - 1] : "") +
+            (str[0] == "1" ? tn[str[1]] : "") +
+            (str[1] != "0" && str[0] != "1" ? " " + dg[str[1]] : "")
+        );
+    } else if (str.length == 3) {
+        return (
+            dg[str[0]] +
+            " " +
+            "hundred" +
+            (str[1] != "0" && str[1] != "1" ? " " + tw[+str[1] - 1] : "") +
+            (str[1] == "1" ? " " + tn[str[2]] : "") +
+            (str[2] != "0" && str[1] != "1" ? " " + dg[str[2]] : "")
+        );
+    }
+};
